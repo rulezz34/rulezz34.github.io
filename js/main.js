7dfcba1d1b5cc427b3931b78667f04ac002567b1
[Geletei Daniel] <[therulezz34@gmail.com]>
2023-04-22 09:33:11 +0200

    new files, new fetures

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// const nav = document.querySelector("nav");
+// const collection = document.getElementsByClassName("menu-class");
+// const menuItems = document.querySelectorAll(".menu-item");
+// const dropdownContent = document.querySelector(".dropdown-content");
+// const dropdownContentP = document.querySelector(".dropdown-a");
+
+// console.log(dropdownContentP);
+
+// window.addEventListener("scroll", function () {
+//   if (document.documentElement.scrollTop > 60) {
+//     nav.classList.add("nav-active");
+//     dropdownContentP.classList.add("dropdown-content-active-p");
+//     dropdownContent.classList.add("dropdown-content-active");
+//     menuItems.forEach((item) => {
+//       item.classList.add("a-active");
+//     });
+//   } else {
+//     nav.classList.remove("nav-active");
+//     dropdownContent.classList.remove("dropdown-content-active");
+//     menuItems.forEach((item) => {
+//       item.classList.remove("a-active");
+//     });
+//   }
+// });
+
+// let li;
+
+// for (let i = 0; i < movies.length; i++) {
+//   li = document.createElement("li");
+//   li.movies[i];
+//   li.classList.add("list-items");
+//   ul.append(li);
+// }
