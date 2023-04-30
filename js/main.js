4d85a81ec27f9d358b834f89766560b2c8418599
[Geletei Daniel] <[therulezz34@gmail.com]>
2023-04-30 23:41:37 +0200

    the very final work

--- js/main.js
+++ js/main.js
@@ -1,33 +1,97 @@
-// const nav = document.querySelector("nav");
-// const collection = document.getElementsByClassName("menu-class");
-// const menuItems = document.querySelectorAll(".menu-item");
-// const dropdownContent = document.querySelector(".dropdown-content");
-// const dropdownContentP = document.querySelector(".dropdown-a");
+const nav = document.querySelector("nav");
+const menuLinks = document.querySelectorAll("nav ul a");
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownLinks = document.querySelectorAll(".dropdown-a");
 
-// console.log(dropdownContentP);
 
-// window.addEventListener("scroll", function () {
-//   if (document.documentElement.scrollTop > 60) {
-//     nav.classList.add("nav-active");
-//     dropdownContentP.classList.add("dropdown-content-active-p");
-//     dropdownContent.classList.add("dropdown-content-active");
-//     menuItems.forEach((item) => {
-//       item.classList.add("a-active");
-//     });
-//   } else {
-//     nav.classList.remove("nav-active");
-//     dropdownContent.classList.remove("dropdown-content-active");
-//     menuItems.forEach((item) => {
-//       item.classList.remove("a-active");
-//     });
-//   }
-// });
 
-// let li;
+window.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop > 60) {
+    nav.classList.add("nav-active");
+    menuLinks.forEach((link) => {
+      link.classList.add("menu-links-active");
+    });
+  } else {
+    nav.classList.remove("nav-active");
+    menuLinks.forEach((link) => {
+      link.classList.remove("menu-links-active");
+    });
+  }
+});
 
-// for (let i = 0; i < movies.length; i++) {
-//   li = document.createElement("li");
-//   li.movies[i];
-//   li.classList.add("list-items");
-//   ul.append(li);
-// }
+window.addEventListener("scroll", () => {
+  let hasMyClass = false;
+
+  menuLinks.forEach((link) => {
+    if (link.classList.contains("menu-links-active")) {
+      hasMyClass = true;
+    }
+  });
+
+  if (hasMyClass) {
+    dropdownContent.classList.add("dropdown-scrolled");
+    dropdownLinks.forEach((link) => {
+      link.classList.add("dropdown-links-active");
+    });
+  } else {
+    dropdownContent.classList.remove("dropdown-scrolled");
+    dropdownLinks.forEach((link) => {
+      link.classList.remove("dropdown-links-active");
+    });
+  }
+});
+
+const btn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".ul-mobile");
+const line1 = document.querySelector(".line-1");
+const line2 = document.querySelector(".line-2");
+const line3 = document.querySelector(".line-3");
+
+btn.addEventListener("click", ()=> {
+  mobileNav.classList.toggle("nav-active-responsive");
+  line1.classList.toggle("line-1-active")
+  line2.classList.toggle("line-2-active")
+  line3.classList.toggle("line-3-active")
+})
+
+function removeNav() {
+  mobileNav.classList.remove("nav-active-responsive");
+  line1.classList.remove("line-1-active")
+  line2.classList.remove("line-2-active")
+  line3.classList.remove("line-3-active")
+}
+
+mobileNav.addEventListener("click", removeNav)
+
+
+function handleScroll() {
+  const scrollY = window.scrollY;
+
+  if (scrollY >= 160) {
+    const animatedElements = document.querySelectorAll(".animate-me");
+    animatedElements.forEach((animatedElement) => {
+      animatedElement.classList.add('slide_up');
+      console.log(animatedElements);
+  });
+}
+
+if (scrollY >= 450) {
+  const animatedElements = document.querySelectorAll(".animate-me-2");
+  animatedElements.forEach((animatedElement) => {
+    animatedElement.classList.add('slide_up');
+    console.log(animatedElements);
+});
+}
+
+if (scrollY >= 1200) {
+  const animatedElements = document.querySelectorAll(".animate-me-3");
+  animatedElements.forEach((animatedElement) => {
+    animatedElement.classList.add('slide_up');
+    console.log(animatedElements);
+});
+}
+}
+
+
+
+window.onscroll = handleScroll;
